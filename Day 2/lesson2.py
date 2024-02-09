efa8a320169a9e7264a029a024608e0e6d27c3ea
anagrigoliagoa321 <anagrigolia80@gmail.com>
2024-02-09 02:52:38 -0800

    Day 2

---
+++ Day 2/lesson2.py
@@ -0,0 +1,16 @@
+print("gavicani da davumegobrdi")
+
+#dima bakuradze
+#mate giorgelashvili
+#irakli kvinchia
+#lika gigiashvili
+#nika gelenidze
+#davit janezashvili
+#barbare tinikashvili
+#davit grdzelishvili
+
+print("ras vfikrob GOA-ze")
+
+#chemtvis goa sauketeso agmochena yvela vinc ki mekitxeba ra aris goa me ubralod veubnebi es aris sauketeso akademia sadac yvela ertmanetis gverdita da swavlac sauketesoa GOA-shi
+
+#madlobaa dedachems ak moxvedrashi man sheitana didi wvlili
