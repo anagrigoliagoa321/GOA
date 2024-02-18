b86e370824b8b81163d0d46018fdb8b71efe4a37
anagrigoliagoa321 <anagrigolia80@gmail.com>
2024-02-18 13:06:11 -0800

    commit

---
+++ Day 05/homeworks/lesson5.py
@@ -0,0 +1,12 @@
+num1 = int(input('Enter number: '))
+num2 = int(input('Enter number: ')) 
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2) # 1 დახრილი ხაზიტ გაყოფა წილადს(float). მაგალითად:(10 / 5 == 2.0)
+print(num1 // num2)#// ორი დახრილი ხაზით გაყოფა გვაძლევს მთელ რიცხვს(integer)
+print(num1 * num2)# * იგივეა რაც გამრავლება
+print(num1 > num2)# > მეტია
+print(num1 < num2)#< ნაკლებია
+print(num1 >= num2)# >= მეტია ან ტოლი
+print(num1 <= num2)# <= ნაკლებია ან ტოლი
