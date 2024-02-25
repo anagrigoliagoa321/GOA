cb2597abb202ed4aa88043fd7723093b0dd845b9
anagrigoliagoa321 <anagrigolia80@gmail.com>
2024-02-25 11:15:38 -0800

    commit

---
+++ Day 07/homeworks/lesson7.py
@@ -0,0 +1,16 @@
+#for i in range(2, 6, 10):
+    #print(i)
+
+#num = 1
+#while num <= 20:
+    #print(num)
+    #num += 1
+
+#count = 0
+#while count <= 5:
+    #print(count)
+    #count += 1
+
+#my_string = "Hello"
+#for char in my_string:
+    #print(char)
